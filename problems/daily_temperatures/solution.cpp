ace516a433d0e607735c469424300bc0ab4c03e6
AbhinavJaintle <86119205+AbhinavJaintle@users.noreply.github.com>
2022-07-21 12:12:09 +0000

    Sync LeetCode submission - Daily Temperatures (cpp)

---
+++ problems/daily_temperatures/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<pair<int,int>> s;
+        vector<int> ans(temperatures.size());
+        for(int i = 0; i<temperatures.size(); i++){
+            if(!s.empty()){
+                while(!s.empty()&&temperatures[i]>s.top().first){
+                    ans[s.top().second]=i-s.top().second;
+                    s.pop();
+                }
+            }
+            if(i==temperatures.size()-1){
+                ans[i] = 0;
+            }
+            else if(temperatures[i]<temperatures[i+1]){
+                ans[i] = 1;
+            }
+            else{
+                s.push(make_pair(temperatures[i],i));
+            }
+        }
+        while(!s.empty()){
+            ans[s.top().second] = 0;
+            s.pop();
+        }
+        return ans;
+    }
+};
