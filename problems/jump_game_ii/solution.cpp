c7bd302d2602ab2380d8e5698155781f203333b0
AbhinavJaintle <86119205+AbhinavJaintle@users.noreply.github.com>
2022-08-03 16:32:02 +0000

    Sync LeetCode submission - Jump Game II (cpp)

---
+++ problems/jump_game_ii/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+        
+    int jump(vector<int>& nums) {
+        if(nums.size()==1){
+            return 0;
+        }
+        vector<int> df(nums.size(),0);
+        df[nums.size()-1]=0;
+        df[nums.size()-2]=1;
+        for(int i = nums.size()-3; i>-1; i--){
+            int points = 10e+4;
+            for(int j = 1; j<=nums[i]&&i+j<nums.size(); j++){
+                points = min(df[i+j],points);
+            }
+            df[i] = 1+points;
+        }
+        return df[0];
+    }
+};
