198f10b54d5cf5dc23f281a73b9d807b314f3105
AbhinavJaintle <86119205+AbhinavJaintle@users.noreply.github.com>
2022-08-31 21:42:02 +0000

    Sync LeetCode submission - Flatten Binary Tree to Linked List (cpp)

--- problems/flatten_binary_tree_to_linked_list/solution.cpp
+++ problems/flatten_binary_tree_to_linked_list/solution.cpp
@@ -9,29 +9,30 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+vector<int> v;
 class Solution {
-    vector<int> v;
 public:
-    void preorder(TreeNode* root){
-        if(root!=NULL){
-            v.push_back(root->val);
-            preorder(root->left);
-            preorder(root->right);
+    void pre(TreeNode* root){
+        if(root==NULL){
+            return;
         }
+        v.push_back(root->val);
+        pre(root->left);
+        pre(root->right);
+        
     }
     void flatten(TreeNode* root) {
-        preorder(root);
+        v.clear();
+        pre(root);
         if(v.size()!=0){
-          
-          root->val = v[0];
-          root->left = NULL;
-          TreeNode* curr = root;
-          root = curr;
-          for(int i =1; i<v.size(); i++){
-            TreeNode* temp = new TreeNode(v[i]);
-            curr->right = temp;
-            curr=temp;
-          } 
-        } 
+            root->left=NULL;
+            TreeNode* curr = root;
+            root=curr;
+            for(int i = 1; i<v.size(); i++){
+                TreeNode* temp = new TreeNode(v[i]);
+                curr->right = temp;
+                curr=temp;
+            }
+        }
     }
 };
