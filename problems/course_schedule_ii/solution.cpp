818436e0e29d9090b2174576595d9e32840fa4c3
AbhinavJaintle <86119205+AbhinavJaintle@users.noreply.github.com>
2022-09-08 12:08:08 +0000

    Sync LeetCode submission - Course Schedule II (cpp)

--- problems/course_schedule_ii/solution.cpp
+++ problems/course_schedule_ii/solution.cpp
@@ -1,42 +1,42 @@
 class Solution {
 public:
-    bool dfs(int index, vector<vector<int>>& adj, vector<int>& vis,vector<int>& ans,vector<int>& ret,map<int,int>& m){
+    bool dfs(int index, vector<vector<int>>& adj, vector<int>& vis, map<int,int>& m, vector<int>& ans){
         if(m[index]==1){
             return true;
         }
         if(vis[index]==1){
-            m[index]=1;
-            ans.push_back(index);
-            return true;
-        }
-        if(ret[index]==1){
             return false;
         }
-        ret[index]=1;
-        for(auto& i: adj[index]){
-            if(dfs(i,adj,vis,ans,ret,m)==false) return false;
-        }
         vis[index]=1;
-        m[index]=1;
+        for(auto&i:adj[index]){
+            if(!dfs(i,adj,vis,m,ans)){
+                return false;
+            }
+        }
         ans.push_back(index);
+        m[index]=1;
         return true;
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
+        vector<int>vis(numCourses,0);
+        vector<vector<int>> adj(numCourses,vector<int>{});
         vector<int> ans;
         map<int,int> m;
-        for(auto& i: prerequisites){
+        for(auto&i:prerequisites){
             adj[i[0]].push_back(i[1]);
         }
-        vector<int> vis(numCourses);
-        vector<int> ret(numCourses);
-        for(auto& i: prerequisites){
-            if(vis[i[0]]==0){
-                if(!dfs(i[0],adj,vis,ans,ret,m)) return {};
+        for(int i = 0; i<adj.size(); i++){
+            if(adj[i].size()==0){
+                m[i]=1;
+                ans.push_back(i);
             }
         }
         for(int i = 0; i<numCourses; i++){
-            if(!m[i]) ans.push_back(i);
+            if(m[i]!=1){
+                if(!dfs(i,adj,vis,m,ans)){
+                    return {};
+                }
+            }
         }
         return ans;
     }
