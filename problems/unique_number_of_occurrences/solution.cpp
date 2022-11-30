d6420c3179e47ac6e224bf7a7ac33dd4d719e3de
AbhinavJaintle <86119205+AbhinavJaintle@users.noreply.github.com>
2022-11-30 04:38:22 +0000

    Sync LeetCode submission - Unique Number of Occurrences (cpp)

---
+++ problems/unique_number_of_occurrences/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int> m1;
+        unordered_map<int,int> m2;
+        for(auto&i:arr){
+            m1[i]++;
+        }
+        for(auto&i:m1){
+            if(m2[i.second]!=0){
+                return false;
+            }
+            m2[i.second]++;
+        }
+        return true;
+    }
+};
